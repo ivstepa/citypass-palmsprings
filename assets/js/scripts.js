1133551da874f1df1f1c06f42682119f21f3e7d1
Ivan Stepanovic <ivstepa@gmail.com>
2020-05-30 02:04:45 +0200

    Adding infowindow on map with directions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -112,171 +112,213 @@ const styles = [
 const locations = {
   activity1: {
     location: { lat: 33.740929, lng: -116.409774 },
-    pin: 'assets/images/green-pin.png'
+    pin: 'assets/images/green-pin.png',
+    description: '<p>Century River Theatre</p><a href="https://www.google.com/maps/dir//Century+at+The+River+and+XD,+71800+CA-111+Ste+A-137,+Rancho+Mirage,+CA+92270,+United+States/@33.7396134,-116.4117286,17z/data=!4m16!1m6!3m5!1s0x80dafdf2ff481b83:0x1b0633ac8289f8d4!2sCentury+at+The+River+and+XD!8m2!3d33.739609!4d-116.4095399!4m8!1m0!1m5!1m1!1s0x80dafdf2ff481b83:0x1b0633ac8289f8d4!2m2!1d-116.4095399!2d33.739609!3e2">Show directions</a></br>'
   },
   activity2: {
     location: { lat: 33.795500, lng: -116.499842 },
-    pin: 'assets/images/orange-pin.png'
+    pin: 'assets/images/orange-pin.png',
+    description: 'Show directions'
   },
   activity3: {
     location: { lat: 33.824580, lng: -116.497739 },
-    pin: 'assets/images/blue-pin.png'
+    pin: 'assets/images/blue-pin.png',
+    description: 'Show directions'
   },
   activity5: {
     location: { lat: 33.833072, lng: -116.546967 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity6: {
     location: { lat: 33.801054, lng: -116.484926 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity7: {
     location: { lat: 33.820023, lng: -116.545981 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity8: {
     location: { lat: 33.819374, lng: -116.546503 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity9: {
     location: { lat: 33.858527, lng: -116.558577 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity10: {
     location: { lat: 33.792484, lng: -116.494132 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity11: {
     location: { lat: 33.824450, lng: -116.549795 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity12: {
     location: { lat: 33.832925, lng: -116.505018 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity13: {
     location: { lat: 33.811504, lng: -116.528309 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity14: {
     location: { lat: 33.823455, lng: -116.528766 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity15: {
     location: { lat: 33.811868, lng: -116.542563 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity16: {
     location: { lat: 33.909292, lng: -116.551905 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity17: {
     location: { lat: 33.849054, lng: -116.547855 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity18: {
     location: { lat: 33.825548, lng: -116.542738 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity19: {
     location: { lat: 33.908052, lng: -116.566898 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity20: {
     location: { lat: 33.823124, lng: -116.546190 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity21: {
     location: { lat: 33.800417, lng: -116.546162 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   activity22: {
     location: { lat: 33.826358, lng: -116.547366 },
-    pin: 'assets/images/pink-pin.png'
+    pin: 'assets/images/pink-pin.png',
+    description: 'Show directions'
   },
   restaurant1: {
     location: { lat: 33.825548, lng: -116.542738 },
-    pin: 'assets/images/blue-pin.png'
+    pin: 'assets/images/blue-pin.png',
+    description: 'Show directions'
   },
   restaurant2: {
     location: { lat: 33.8228005, lng: -116.5784875 },
-    pin: 'assets/images/blue-pin.png'
+    pin: 'assets/images/blue-pin.png',
+    description: 'Show directions'
   },
   restaurant3: {
     location: { lat: 33.8237328, lng: -116.5494426 },
-    pin: 'assets/images/blue-pin.png'
+    pin: 'assets/images/blue-pin.png',
+    description: 'Show directions'
   },
   restaurant4: {
     location: { lat: 33.8224049, lng: -116.5482715 },
-    pin: 'assets/images/blue-pin.png'
+    pin: 'assets/images/blue-pin.png',
+    description: 'Show directions'
   },
   restaurant5: {
     location: { lat: 33.83418, lng: -116.5486948 },
-    pin: 'assets/images/blue-pin.png'
+    pin: 'assets/images/blue-pin.png',
+    description: 'Show directions'
   },
   restaurant6: {
     location: { lat: 33.8326959, lng: -116.5487097 },
-    pin: 'assets/images/blue-pin.png'
+    pin: 'assets/images/blue-pin.png',
+    description: 'Show directions'
   },
   restaurant7: {
     location: { lat: 33.8210364, lng: -116.5486592 },
-    pin: 'assets/images/blue-pin.png'
+    pin: 'assets/images/blue-pin.png',
+    description: 'Show directions'
   },
   trail1: {
     location: { lat: 33.8171946, lng: -116.5270641 },
-    pin: 'assets/images/green-pin.png'
+    pin: 'assets/images/green-pin.png',
+    description: 'Show directions'
   },
   trail2: {
     location: { lat: 33.7497215, lng: -116.5431385 },
-    pin: 'assets/images/green-pin.png'
+    pin: 'assets/images/green-pin.png',
+    description: 'Show directions'
   },
   trail3: {
     location: { lat: 33.8099974, lng: -116.5549416 },
-    pin: 'assets/images/green-pin.png'
+    pin: 'assets/images/green-pin.png',
+    description: 'Show directions'
   },
   trail4: {
     location: { lat: 33.8236881, lng: -116.5516245 },
-    pin: 'assets/images/green-pin.png'
+    pin: 'assets/images/green-pin.png',
+    description: 'Show directions'
   },
   trail5: {
     location: { lat: 33.7497215, lng: -116.5431385 },
-    pin: 'assets/images/green-pin.png'
+    pin: 'assets/images/green-pin.png',
+    description: 'Show directions'
   },
   trail6: {
     location: { lat: 33.7933265, lng: -116.5652725 },
-    pin: 'assets/images/green-pin.png'
+    pin: 'assets/images/green-pin.png',
+    description: 'Show directions'
   },
   trail7: {
     location: { lat: 33.820415, lng: -116.548699 },
-    pin: 'assets/images/green-pin.png'
+    pin: 'assets/images/green-pin.png',
+    description: 'Show directions'
   },
   instagrammable1: {
     location: { lat: 33.7952328, lng: -116.50202 },
-    pin: 'assets/images/orange-pin.png'
+    pin: 'assets/images/orange-pin.png',
+    description: 'Show directions'
   },
   instagrammable2: {
     location: { lat: 33.8171946, lng: -116.5270641 },
-    pin: 'assets/images/orange-pin.png'
+    pin: 'assets/images/orange-pin.png',
+    description: 'Show directions'
   },
   instagrammable3: {
     location: { lat: 33.8328845, lng: -116.549177 },
-    pin: 'assets/images/orange-pin.png'
+    pin: 'assets/images/orange-pin.png',
+    description: 'Show directions'
   },
   instagrammable4: {
     location: { lat: 33.819827, lng: -116.5481482 },
-    pin: 'assets/images/orange-pin.png'
+    pin: 'assets/images/orange-pin.png',
+    description: 'Show directions'
   },
   instagrammable5: {
     location: { lat: 33.8191686, lng: -116.5486591 },
-    pin: 'assets/images/orange-pin.png'
+    pin: 'assets/images/orange-pin.png',
+    description: 'Show directions'
   },
   instagrammable6: {
     location: { lat: 33.8242, lng: -116.552037 },
-    pin: 'assets/images/orange-pin.png'
+    pin: 'assets/images/orange-pin.png',
+    description: 'Show directions'
   },
   instagrammable7: {
     location: { lat: 33.7497215, lng: -116.5431385 },
-    pin: 'assets/images/orange-pin.png'
+    pin: 'assets/images/orange-pin.png',
+    description: 'Show directions'
   }
 };
 
@@ -290,7 +332,8 @@ class GMap {
       this.markers[key] = new google.maps.Marker({
         position: info.location,
         map: this.map,
-        icon: info.pin
+        icon: info.pin,
+        description: info.description
       });
     });
   }
@@ -299,12 +342,15 @@ class GMap {
     this.locationId = locationId;
 
     const marker = this.markers[locationId];
-    console.log(marker);
+    const infowindow = new google.maps.InfoWindow({
+      content: marker.description
+    });
 
     marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(() => marker.setAnimation(null), 2000);
     this.map.panTo(this.locations[locationId].location);
     this.map.setZoom(16);
+    infowindow.open(map, marker);
   }
 }
 
